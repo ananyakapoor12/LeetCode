6975d20dae2e2cfe23d615c55649d40a5622f459
Ananya Kapoor <ananya.kapoor.1103@gmail.com>
2024-07-05 18:04:16 +0800

    Delete 169.java

--- 169.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int majorityElement(int[] nums) 
-    {
-        Arrays.sort(nums); // sort array
-        int n = nums.length;
-        return nums[n/2]; // majority always middle element acc to definition 
-           
-    }
-}
